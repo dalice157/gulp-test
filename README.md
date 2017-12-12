7c0a3c141375607c0e9c9724483fb3cacb1ca23a
dalice.chu <dalice.chu@104.com.tw>
2017-12-12 18:09:31 +0800

    新增gulp API說明

--- README.md
+++ README.md
@@ -47,6 +47,7 @@ $ npm install
 ```
 #### 3. **在專案下的根目錄建立 `gulpfile.js`**
 安裝完後在根目錄下創建一個 `gulpfile.js`，之後只要在專案下執行 `gulp` 後，`gulp` 會直接讀取 `gulpfile.js` 檔案去運行。
+**預設寫法：**
 ```
 // 引用 Gulp plugin
 var gulp = require('gulp');
@@ -56,13 +57,30 @@ gulp.task('default', function () {
     console.log('Hi~Welcome~gulp!!');
 });
 ```
-執行 `gulp` 的話會運行 **default** 的任務，除非 **task** 有新增別的任務，且直接執行新增的任務，ex: `gulp scss`，否則都會執行 **default** 這個任務。
+執行 `gulp` 的話會運行 **default** 的任務，除非 **task** 有新增別的任務，且直接執行新增的任務，以此專案為例 `gulp scss`，否則都會執行 **default** 這個任務。
 ```
-$ gulp
+$ gulp //執行 default
+$ gulp scss //執行 scss 編譯
 
 // Gulp 後的訊息
 [22:26:31] Using gulpfile ~/Users/dalice.chu/Desktop/gulp_test/gulpfile.js
 [22:26:31] Starting 'default'...
 Hi~Welcome~gulp!!
 [22:26:31] Finished 'default' after 126 μs
+```
+## Gulp API 說明
+### gulp.task(name, [deps], fn)
++ **說明：**用在定義一個任務，一個 `task` 為一個任務。
++ **name：**任務名稱（必填，所以不能為空）。
++ **deps：**將要依賴的任務全部都包在此。(被依賴的任務需要返回當前任務的事件流)
++ **fn：**該任務調用的套件操作。
+```
+gulp.task('default', ['watch', 'scss', 'webserver']);
+
+gulp.task('scss', function () {
+  return gulp.src('scss/**/*.scss') 
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(gulp.dest('css'));
+});
+
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
 // 載入 gulp
 var gulp = require('gulp');
-var sass = require('gulp-sass'); // 載入 sass 編譯器
+var sass = require('gulp-sass');            // 載入 sass 編譯器
 var webserver = require('gulp-webserver');  // 載入 webserver
 
 // 定義名稱為 default 的 gulp 工作
 gulp.task('default', ['watch', 'scss', 'webserver']);
 
 gulp.task('scss', function () {
-  return gulp.src('scss/**/*.scss') // sass 的來源資料夾
-	.pipe(sass(                     // 編譯 sass
-		{outputStyle: 'expanded'}     // sass 的輸出格式
+  return gulp.src('scss/**/*.scss')  // sass 的來源資料夾
+	.pipe(sass(                        // 編譯 sass
+		{outputStyle: 'expanded'}        // sass 的輸出格式
 		).on('error', sass.logError)
 	)
-	.pipe(gulp.dest('css')); // sass 編譯完成後的匯出資料夾
+	.pipe(gulp.dest('css'));           // sass 編譯完成後的匯出資料夾
 });
 
 // webserver
 gulp.task('webserver', function() {
   setTimeout(function(){
     gulp.src('./')                   // 預設開啟路徑
-      .pipe(webserver({                     // 啟動 webserver
-        livereload: true,                   // Livereload 的功能
-        open: false,                        // 是否自動開啟 瀏覽器
-        host: '0.0.0.0',                    // 如果使用 0.0.0.0 的 ip，還會另外開啟 wifi 等對外網路
-        port: 10000,                        // 開放通訊埠
+      .pipe(webserver({              // 啟動 webserver
+        livereload: true,            // Livereload 的功能
+        open: false,                 // 是否自動開啟 瀏覽器
+        host: '0.0.0.0',             // 如果使用 0.0.0.0 的 ip，還會另外開啟 wifi 等對外網路
+        port: 10000,                 // 開放通訊埠
       }));
   }, 1000);
 });
